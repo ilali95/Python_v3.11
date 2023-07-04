450b40bdef673b35b281ac65f9a947555406353a
ilali95 <ilali.unfeeling@mail.ru>
2023-07-04 17:01:51 +0300

    .

--- HW_Python/hw05/hw02.py
+++ HW_Python/hw05/hw02.py
@@ -3,3 +3,13 @@
 # указанием процентов вида «10.25%». В результате получаем словарь с 
 # именем в качестве ключа и суммой
 # премии в качестве значения. Сумма рассчитывается
+
+def generate_dictionary(names, rates, bonuses):
+    return {name: round(rate * float(bonus.rstrip('%')) / 100, 2) for name, rate, bonus in zip(names, rates, bonuses)}
+
+names = ["Алиса", "Боб", "Вася"]
+rates = [100, 200, 150]
+bonuses = ["10.25%", "5.5%", "8.75%"]
+
+result = generate_dictionary(names, rates, bonuses)
+print(result)
